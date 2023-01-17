ec0dce90b8a76db28cdc8b02d0c719c08d5729a7
sethbanta <sethbanta@u.boisestate.edu>
2023-01-16 17:54:29 -0700

    Adds new constructors for locations and quests
    Added a new constructor for both locations and quests and testing adding a new location with its constructor.

--- logic/Location.cs
+++ logic/Location.cs
@@ -11,6 +11,13 @@ namespace logic
         public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        
+        public Location(int id, string name, string description)
+        {
+            ID = id;
+            Name = name;
+            Description = description;
+        }
 
     }
 }
