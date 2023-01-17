724bd34eac85e3adf0eb19ee0a640d95daabf52f
sethbanta <sethbanta@u.boisestate.edu>
2023-01-16 18:04:43 -0700

    Added new constructors for MOST classes
    Added new constructors for every class that logic uses except for locations and quests since I added those in the last commit

--- logic/LivingCreature.cs
+++ logic/LivingCreature.cs
@@ -10,5 +10,11 @@ namespace logic
     {
         public int CurrentHitPoints { get; set; }
         public int MaximumHitPoints { get; set; }
+
+        public LivingCreature(int currentHitPoints, int maximumHitPoints)
+        {
+            CurrentHitPoints = currentHitPoints;
+            MaximumHitPoints = maximumHitPoints;
+        }
     }
 }
