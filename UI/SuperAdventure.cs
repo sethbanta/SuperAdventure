f6058b9b7337f11f2efbfd899497feaf0c961b9b
sethbanta <sethbanta@u.boisestate.edu>
2023-01-16 17:32:47 -0700

    Adds initialization of the player
    Initializes the variables and stats used for the player throughout the game and then loads them into the window

--- UI/SuperAdventure.cs
+++ UI/SuperAdventure.cs
@@ -7,21 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using logic;
 
 namespace UI
 {
     public partial class SuperAdventure : Form
     {
+        private Player _player;
         public SuperAdventure()
         {
             InitializeComponent();
+            _player = new Player();
+            _player.currentHitPoints = 10;
+            _player.maximumHitPoints = 20;
+            _player.gold = 20;
+            _player.experiencePoints = 0;
+            _player.level = 1;
+            lblHitPoints.Text = _player.currentHitPoints.ToString();
+            lblGold.Text = _player.gold.ToString();
+            lblExperience.Text = _player.experiencePoints.ToString();
+            lblLevel.Text = _player.level.ToString();
         }
 
         private void SuperAdventure_Load(object sender, EventArgs e)
         {
 
         }
-
-
     }
 }
