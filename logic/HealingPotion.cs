78b166b0e7f2aa769d1a81223a3bc178fe5bab73
sethbanta <sethbanta@u.boisestate.edu>
2023-01-16 17:44:29 -0700

    Modified HealingPotion and Weapon to be items
    Modified the healing potion and weapon classes so that they are children of the item class

--- logic/HealingPotion.cs
+++ logic/HealingPotion.cs
@@ -8,9 +8,6 @@ namespace logic
 {
     public class HealingPotion : Item
     {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string NamePlural { get; set; }
         public int AmountToHeal { get; set; }
     }
 }
