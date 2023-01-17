8db4907acb50e9bebd2f70a3eda7cb71dd1afe39
sethbanta <sethbanta@u.boisestate.edu>
2023-01-16 17:46:45 -0700

    Created LivingCreature class who is a parent
    Created a LivingCreature class to be used as a parent for the monster and player class to inherit the hitpoint variables

---
+++ logic/LivingCreature.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logic
+{
+    public class LivingCreature
+    {
+        public int CurrentHitPoints { get; set; }
+        public int MaximumHitPoints { get; set; }
+    }
+}
